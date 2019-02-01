e71341bf05d8561dfb6231cbe688014a84ed29a2
George Kiragu <45111839+GeorMurk@users.noreply.github.com>
2019-02-01 20:02:46 +0300

    Add files via upload

---
+++ Variable.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+#!Working with Variables
+
+character_name = "Jason"
+character_age = "99"
+
+#!when using boolean the following code follows taking a statement where True or False
+
+is_male = True
+
+print("Back in the day there was a guy called " + character_name)
+print(character_name + " was " + character_age + " years old")
+
+
+#!Go ahead and test!!!!!
